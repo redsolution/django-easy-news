f4a002538b6a3b321b487f7fca852c4680a3c516
Alexander Ivanov <alexander.ivanov@redsolution.ru>
2010-09-07 20:10:28 +0600

    Fix templates for CMS

--- easy_news/redsolution_setup/admin.py
+++ easy_news/redsolution_setup/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from easy_news.redsolution_setup.models import EasyNewsSettings
+from redsolutioncms.admin import CMSBaseAdmin
+
+class EasyNewsSettingsAdmin(CMSBaseAdmin):
+    model = EasyNewsSettings
 
 try:
     admin.site.register(EasyNewsSettings)
